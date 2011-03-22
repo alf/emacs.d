c4aee3768422fdb19780561548b429bcbcc6b48e
Alf Lervag <alf.lervag@bouvet.no>
2011-03-22 13:48:58 +0100

    Make diff unified by default

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(diff-switches "-u")
  '(enable-recursive-minibuffers t)
  '(find-grep-options "-q  --exclude=\"*.svn-base\" --exclude=\"*~\"")
  '(inhibit-startup-screen t)
