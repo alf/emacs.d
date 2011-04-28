b107553bfd67de1cfb256b8da9c21830b466ac8f
Alf Lervag <alf.lervag@bouvet.no>
2011-04-28 10:41:02 +0200

    Make python-mode use python as the compile command.  This lets me create simple unittests in my python files and then easily run them.

--- scripts/init-modes.el
+++ scripts/init-modes.el
@@ -5,3 +5,7 @@
 (setq auto-mode-alist
 	(cons '("\\.pt\\'" . nxml-mode)
 		auto-mode-alist))
+
+(add-hook 'python-mode-hook
+	  (lambda()
+	    (set (make-local-variable 'compile-command) (concat "python " (buffer-name)))))
