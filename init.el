b0c5500711a87439ae7e602f9e32b177f0bf5bd9
Alf Lervag <alf.lervag@bouvet.no>
2012-01-31 09:53:50 +0100

    Added a beautify-function for json

--- init.el
+++ init.el
@@ -139,6 +139,13 @@ by using nxml's indentation rules."
     (pop-mark))
   (message "Ah, much better!"))
 
+(defun beautify-json ()
+  (interactive)
+  (let ((b (if mark-active (min (point) (mark)) (point-min)))
+        (e (if mark-active (max (point) (mark)) (point-max))))
+    (shell-command-on-region b e
+     "python -mjson.tool" (current-buffer) t)))
+
 (defun open-index (id)
   (interactive "sEnter index-id: ")
   (let ((index-url (concat
