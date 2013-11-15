1ad3c82fc91ac2e45a71cd97778ea4ef404c9d74
Alf Lervåg <alf@krupa.bouvet.no>
2013-11-15 13:18:32 +0100

    Adding support for multiple accounts to mu4e

--- setup-mu4e.el
+++ setup-mu4e.el
@@ -2,4 +2,58 @@
 (setq mu4e-view-show-images t)
 (setq mu4e-html2text-command "html2text -utf8 -width 72 -nobs")
 
+(defvar my-mu4e-account-alist
+  '(("Bouvet"
+     (mu4e-sent-folder "/Bouvet/Sent Items")
+     (mu4e-drafts-folder "/Bouvet/Drafts")
+     (user-mail-address "alf.lervag@bouvet.no")
+     (message-signature-file ".Bouvet.txt")
+     (smtpmail-default-smtp-server "mail.bouvet.no")
+     (smtpmail-local-domain "bouvet.no")
+     (smtpmail-smtp-server "mail.bouvet.no")
+     (smtpmail-stream-type starttls)
+     (smtpmail-smtp-service 465))
+    ("Lervag"
+     (mu4e-sent-folder "/Lervag/sent_messages")
+     (mu4e-drafts-folder "/Lervag/drafts")
+     (user-mail-address "alf@lervag.net")
+     (message-signature-file ".Lervag.txt")
+     (smtpmail-default-smtp-server "smtp.gmail.com")
+     (smtpmail-local-domain "lervag.net")
+     (smtpmail-smtp-server "smtp.gmail.com")
+     (smtpmail-stream-type starttls)
+     (smtpmail-smtp-service 587))
+    ("GMail"
+     (mu4e-sent-folder "/GMail/sent_messages")
+     (mu4e-drafts-folder "/GMail/drafts")
+     (user-mail-address "alfborge@gmail.com")
+     (message-signature-file ".GMail.txt")
+     (smtpmail-default-smtp-server "smtp.gmail.com")
+     (smtpmail-local-domain "gmail.com")
+     (smtpmail-smtp-server "smtp.gmail.com")
+     (smtpmail-stream-type starttls)
+     (smtpmail-smtp-service 587))))
+
+(defun my-mu4e-set-account (&optional account)
+  "Set the account for composing a message."
+  (let* ((account
+          (or account
+              (if mu4e-compose-parent-message
+                  (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+                    (string-match "/\\(.*?\\)/" maildir)
+                    (match-string 1 maildir))
+                (completing-read (format "Compose with account: (%s) "
+                                         (mapconcat #'(lambda (var) (car var)) my-mu4e-account-alist "/"))
+                                 (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
+                                 nil t nil nil (caar my-mu4e-account-alist)))))
+         (account-vars (cdr (assoc account my-mu4e-account-alist))))
+    (if account-vars
+        (mapc #'(lambda (var)
+                  (set (car var) (cadr var)))
+              account-vars)
+      (error "No email account found"))))
+
+(my-mu4e-set-account "Bouvet")
+(add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
+
 (provide 'setup-mu4e)
