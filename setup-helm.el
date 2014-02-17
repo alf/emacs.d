b9e5bd4c4f3bbc8e31ddfb6cd40500fcda771db7
Alf Lervag <alf.lervag@bouvet.no>
2014-02-17 12:52:01 +0100

    More helm-stuff

--- setup-helm.el
+++ setup-helm.el
@@ -1,6 +1,8 @@
 (require 'helm-config)
+(require 'helm-ls-git)
 
 (global-set-key (kbd "C-c h") 'helm-mini)
+(global-set-key (kbd "C-x C-d") 'helm-browse-project)
 (helm-mode 1)
 
 (provide 'setup-helm)
