33ebf6dc6776767e0870e994dd9d71766243a2ba
Alf Lervag <alf.lervag@bouvet.no>
2014-02-19 19:10:29 +0100

    Added in helm-prelude
    Stole a function from prelude that uses helm-projectile if we're in a
    projectile root and helm-mini otherwise.
    
    https://github.com/bbatsov/prelude/blob/master/modules/prelude-helm.el

--- setup-helm.el
+++ setup-helm.el
@@ -9,7 +9,18 @@
 (require 'helm-ls-git)
 (require 'helm-config)
 
-(global-set-key (kbd "C-c h") 'helm-mini)
+(defun helm-prelude ()
+  "Preconfigured `helm'."
+  (interactive)
+  (condition-case nil
+      (if (projectile-project-root)
+          (helm-projectile)
+        ;; otherwise fallback to `helm-mini'
+        (helm-mini))
+    ;; fall back to helm mini if an error occurs (usually in `projectile-project-root')
+    (error (helm-mini))))
+
+(global-set-key (kbd "C-c h") 'helm-prelude)
 (global-set-key (kbd "C-x C-d") 'helm-projectile)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x c a") 'helm-ag)
