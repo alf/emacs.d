dc994c1ee559f63070d968c7481b06ee4279486e
Alf Lervag <alf.lervag@bouvet.no>
2014-02-20 12:57:02 +0100

    Some more tweaks

--- setup-evil.el
+++ setup-evil.el
@@ -5,6 +5,7 @@
 (define-key evil-insert-state-map (kbd "C-SPC") 'evil-normal-state)
 
 ;; Doesn't seem to work, but we'll keep it for now so I know that I've tried it.
-(evil-set-initial-state org-capture-mode 'insert)
+;(evil-set-initial-state 'org-capture-mode 'insert)
+(evil-set-initial-state 'magit-process-mode 'emacs)
 ;(add-hook 'org-capture-mode-hook 'evil-append-line)
 (provide 'setup-evil)
