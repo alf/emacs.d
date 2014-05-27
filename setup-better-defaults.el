a60ed9eee6a89f5d262d7f3dbfb3ddef8e48df41
Alf Lervag <alf.lervag@bouvet.no>
2014-05-27 14:26:16 +0200

    Show weeknumbers in calendar

--- setup-better-defaults.el
+++ setup-better-defaults.el
@@ -12,4 +12,16 @@
 
 (add-to-list 'after-save-hook 'delete-trailing-whitespace)
 
+;; Show week numbers in calendar
+(copy-face font-lock-constant-face 'calendar-iso-week-face)
+(set-face-attribute 'calendar-iso-week-face nil
+                    :height 0.7)
+(setq calendar-intermonth-text
+      '(propertize
+        (format "%2d"
+                (car
+                 (calendar-iso-from-absolute
+                  (calendar-absolute-from-gregorian (list month day year)))))
+        'font-lock-face 'calendar-iso-week-face))
+
 (provide 'setup-better-defaults)
