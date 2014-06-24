a185bf849a8252db95ced93df5751bc1304ad2b9
Alf Lervag <alf.lervag@bouvet.no>
2014-06-24 12:34:01 +0200

    Change colors of modeline based on mode

--- lisp/setup-evil.el
+++ lisp/setup-evil.el
@@ -11,4 +11,18 @@
 (evil-set-initial-state 'dired-mode 'emacs)
 (evil-set-initial-state 'git-commit-mode 'emacs)
 (evil-set-initial-state 'help-mode 'emacs)
+
+;; change mode-line color by evil state
+(lexical-let ((default-color (cons (face-background 'mode-line)
+                                   (face-foreground 'mode-line))))
+  (add-hook 'post-command-hook
+    (lambda ()
+      (let ((color (cond ((minibufferp) default-color)
+                         ((evil-insert-state-p) '("#e80000" . "#ffffff"))
+                         ((evil-emacs-state-p)  '("#444488" . "#ffffff"))
+                         ((buffer-modified-p)   '("#006fa0" . "#ffffff"))
+                         (t default-color))))
+        (set-face-background 'mode-line (car color))
+        (set-face-foreground 'mode-line (cdr color))))))
+
 (provide 'setup-evil)
