3910a6a6750e3fcd38ac621576d5610168287d16
Alf Lervag <alf.lervag@bouvet.no>
2014-08-21 09:53:30 +0200

    Use flake8 for python linting

--- custom.el
+++ custom.el
@@ -3,9 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ede-project-directories (quote ("/Users/alf/Projects/kunde/mattilsynet/arkivgrensesnitt")))
+ '(ede-project-directories
+   (quote
+    ("/Users/alf/Projects/kunde/mattilsynet/arkivgrensesnitt")))
  '(enable-recursive-minibuffers t)
- '(erc-nick "alfborge" t)
+ '(erc-nick "alfborge")
  '(erc-port 6667)
  '(erc-prompt-for-password nil)
  '(inhibit-startup-screen t)
@@ -15,7 +17,8 @@
  '(ns-alternate-modifier (quote none))
  '(ns-command-modifier (quote meta))
  '(nxml-child-indent 4)
- '(org-babel-clojure-backend (quote cider))
+ '(org-babel-clojure-backend (quote cider) t)
+ '(python-check-command "flake8")
  '(send-mail-function (quote mailclient-send-it))
  '(session-use-package t nil (session))
  '(visible-bell t))
@@ -27,4 +30,3 @@
  '(jira-comment-face ((t (:background "gray95"))))
  '(jira-issue-info-face ((t (:background "cornsilk" :foreground "black"))))
  '(org-mode-line-clock ((t (:foreground "red" :box (:line-width -1 :style released-button)))) t))
-
