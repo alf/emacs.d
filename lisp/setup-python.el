aba1b35e4aefbf2880b00d55d034d257273ed4b3
Alf Lervag <alf.lervag@bouvet.no>
2014-08-21 09:51:35 +0200

    New version of elpy dropped this nice feature

--- lisp/setup-python.el
+++ lisp/setup-python.el
@@ -2,5 +2,25 @@
 (require-package 'elpy)
 (elpy-enable)
 
+(defun elpy-nav-forward-definition ()
+  "Move forward to the next definition (class or function)."
+  (interactive)
+  (if (save-excursion
+        (forward-char 1)
+        (re-search-forward "^ *\\(def\\|class\\) " nil t))
+      (goto-char (match-beginning 1))
+    (goto-char (point-max))))
+
+(defun elpy-nav-backward-definition ()
+  "Move backward to the previous definition (class or function)."
+  (interactive)
+  (if (save-excursion
+        (forward-char -1)
+        (re-search-backward "^ *\\(def\\|class\\) " nil t))
+      (goto-char (match-beginning 1))
+    (goto-char (point-min))))
+
+(define-key elpy-mode-map (kbd "M-n") 'elpy-nav-forward-definition)
+(define-key elpy-mode-map (kbd "M-p") 'elpy-nav-backward-definition)
 
 (provide 'setup-python)
