e1cf082baee3ed8062c913dda5e54c95a5863ecf
Alf Lervag <alf.lervag@bouvet.no>
2014-08-27 16:32:26 +0200

    Started to use projectile more

--- lisp/setup-projectile.el
+++ lisp/setup-projectile.el
@@ -1,4 +1,12 @@
 (require-package 'projectile)
 
+(setq projectile-completion-system 'helm)
 (projectile-global-mode)
+
+;; Prefer this one since it gives me helm actions
+(define-key projectile-mode-map (kbd "C-c p p") 'helm-projectile-switch-project)
+
+;; This lets me use fig run to run tests in projectile
+(add-to-list 'projectile-project-root-files-bottom-up "fig.yml")
+
 (provide 'setup-projectile)
