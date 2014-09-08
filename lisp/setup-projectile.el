23a4e8fede7470bb9d3bfa6aba16847fb17e0b1c
Alf Lervag <alf.lervag@bouvet.no>
2014-09-08 09:11:31 +0200

    Prefer to create a .projectile file

--- lisp/setup-projectile.el
+++ lisp/setup-projectile.el
@@ -16,7 +16,4 @@
 (define-key projectile-mode-map (kbd "C-c p p") 'helm-projectile-switch-project)
 (setq projectile-switch-project-action 'alf/helm-projectile-find-files)
 
-;; This lets me use fig run to run tests in projectile
-(add-to-list 'projectile-project-root-files-bottom-up "fig.yml")
-
 (provide 'setup-projectile)
