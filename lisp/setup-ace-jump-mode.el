667f7128af2ac90b5704b25a329e19877430b014
Alf Lervag <alf.lervag@bouvet.no>
2014-09-10 12:53:19 +0200

    oe was a bad binding, 'noe' is too common a word

--- lisp/setup-ace-jump-mode.el
+++ lisp/setup-ace-jump-mode.el
@@ -1,6 +1,6 @@
 (require-package 'ace-jump-mode)
 (global-set-key (kbd "C-x SPC")  'ace-jump-mode)
 
-(key-chord-define-global "oe"     'ace-jump-mode)
+(key-chord-define-global "xb"     'ace-jump-mode)
 
 (provide 'setup-ace-jump-mode)
