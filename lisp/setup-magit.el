a8e2560d0a89966185d56ee12622a0a19e57d91b
Alf Lervag <alf.lervag@bouvet.no>
2014-09-11 21:38:42 +0200

    Added magit pull requests support

--- lisp/setup-magit.el
+++ lisp/setup-magit.el
@@ -28,4 +28,19 @@
   (setq magit-refresh-args `(oneline "HEAD" ("--graph" "--" ,dir-name)))
   (magit-refresh))
 
+(unless (fboundp 'magit-gh-pulls-mode)
+  (package-install 'magit-gh-pulls))
+
+(eval-after-load 'magit
+  '(define-key magit-mode-map "#gg"
+     'endless/load-gh-pulls-mode))
+
+(defun endless/load-gh-pulls-mode ()
+  "Start `magit-gh-pulls-mode' only after a manual request."
+  (interactive)
+  (require 'magit-gh-pulls)
+  (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
+  (magit-gh-pulls-mode 1)
+  (magit-gh-pulls-reload))
+
 (provide 'setup-magit)
